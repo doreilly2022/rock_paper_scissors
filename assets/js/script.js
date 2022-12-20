f116adbb721c44f636551546762ba55e154e048c
doreilly2022 <doreilly@hubspot.com>
2022-12-20 20:36:42 +0000

    Add function and if else statements for each scenario

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,52 @@
 
-const options = document.querySelectorAll(".rps");
+const options = document.querySelectorAll(".rps_options");
 let player_score = 0;
 let comp_score = 0;
 
+options.forEach((option) => {
+    option.addEventLister("click", function () {
+        const pInput = this.textContent;
+
+        const cOptions = ["Rock", "Paper", "Scissors"];
+        const cInput = cOptions[Math.floor(Math.random () * 3)];
+
+        compareInputs (pInput, cInput);
+    }); 
+});
+
+function compareInputs (pInput, cInput) {
+    const currentMatch = `${pInput} vs ${cInput}`;
+    // Draw
+    if (pInput === cInput) {
+        alert(`${currentMatch} is a Tie`);
+        return;
+    }
+
+    //Rock
+    if (pInput === "Rock") {
+        if (cInput === "Scissors") {
+            alert (`${currentMatch} = You Win`)
+        } else {
+            alert (`${currentMatch} = Computer Wins`)
+        }
+    }
+
+    //Paper
+    else if (pInput === "Paper") {
+        if (cInput === "Rock") {
+            alert (`${currentMatch} = You Win`)
+        } else {
+            alert (`${currentMatch} = Computer Wins`)
+        }
+    }
+
+    //Scissors
+    else if (pInput === "Scissors") {
+        if (cInput === "Paaper") {
+            alert (`${currentMatch} = You Win`)
+        } else {
+            alert (`${currentMatch} = Computer Wins`)
+        }
+    }
+
+}
